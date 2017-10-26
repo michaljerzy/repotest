ec84a8265b7cd95c07823f1eba1ea737245be439
michaljerzy <michal.jerzy99@gmail.com>
2017-10-26 08:00:15 +0200

    .

--- SQL/Zadanie 3/sqlite3_2/andrzejewski_szkola.py
+++ SQL/Zadanie 3/sqlite3_2/andrzejewski_szkola.py
@@ -4,23 +4,22 @@
 #  baza_sql.py
 
 import sqlite3
-
-def wyniki(cur):
     
+    
+def wyniki(cur):
+    wyniki = cur.fetchall() #pobierz wszystkie wiersze
     for row in wyniki:
         print(tuple(row))
         
 def kw_a(cur):
     cur.execute("""
-        SELECT  Imie, Nazwisko, tbKlasy.Klasa
-        FROM tbUczniowie
+        SELECT  Imie, Nazwisko, Klasa
+        FROM tbUczniowie, tbKlasy
         WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
         AND tbKlasy.Klasa = '1A'
     """) 
-    wyniki (cur.fetchall())
-
+    wyniki(cur)
     
-
 def kw_b(cur):
         
     cur.execute("""
@@ -28,34 +27,48 @@ def kw_b(cur):
         FROM tbUczniowie
     """ )
     
-    wyniki (cur.fetchall())
+    wyniki(cur)
   
 def kw_c(cur):
         
     cur.execute("""
-        SELECT AVG(EgzHum)
+        SELECT AVG(EgzMat)
         FROM tbUczniowie, tbKlasy
         WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
         AND tbKlasy.Klasa = '1A'
     """ )
     
-    wyniki (cur.fetchall())
+    wyniki(cur)
     
 def kw_d(cur):
     cur.execute("""
         SELECT Imie, Nazwisko, Ocena
         From tbUczniowie, tbOceny
         WHERE tbOceny.UczenID = tbUczniowie.IDUcznia
-        
-        
+        AND tbUczniowie.Nazwisko = 'Nowak'
     """ )
     
+    wyniki(cur)
+    
+def kw_e(cur):
+    cur.execute("""
+    SELECT AVG(tbOceny.Ocena)
+    FROM tbOceny, tbPrzedmioty
+    WHERE tbOceny.PrzedmiotID = tbPrzedmioty.IDPrzedmiotu
+    AND tbPrzedmioty.Przedmiot = 'fizyka'
+    AND tbOceny.Datad > '2012-10-01'
+    AND tbOceny.Datad < '2012-10-31'
+    """ )
+    wyniki(cur)
     
 def main(args):
     con = sqlite3.connect('szkola.db')
     cur = con.cursor() #utworzenie kursora
-    
+    kw_a(cur)
+    kw_b(cur)
     kw_c(cur)
+    kw_d(cur)
+    kw_e(cur)
     
       
     return 0
---
+++ git_myk.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def f(a, b):
+    return (a + b)
+
+def main(args):
+    adr = '/home/kl3ag2/github/Informatyka/'
+    #os.system(f('cd %s', adr))
+    os.system('git add .')
+    log = os.system('git commit -m .')
+    if log != 256:
+        os.system('git push')
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    import os
+sys.exit(main(sys.argv))
