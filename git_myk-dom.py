244a0a76e9b66a547da7c8b0c9fba3cc439d946f
michaljerzy <michal.jerzy99@gmail.com>
2017-11-11 10:34:43 +0100

    .

---
+++ git_myk-dom.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def f(a, b):
+    return (a + b)
+
+def main(args):
+    #adr = '/michal/git/repotest/'
+    #os.system(f('cd %s', adr))
+    os.system( 'git status')
+    print('\n')
+    os.system('git add .')
+    log = os.system('git commit -m .')
+    if log != 256:
+        os.system('git push')
+        print('\n')
+    os.system( 'git status')
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    import os
+sys.exit(main(sys.argv))
