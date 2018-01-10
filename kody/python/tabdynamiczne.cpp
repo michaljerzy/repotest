96e46bcc440a6c1f13ad798efb76e4009daae816
michaljerzy <michaljerzy99@gmail.com>
2018-01-10 13:29:35 +0100

    .

--- kody/python/tabdynamiczne.cpp
+++ kody/python/tabdynamiczne.cpp
@@ -4,21 +4,64 @@
 
 using namespace std;
 
+
+void wprowadz(float *t, int ile) {
+    for(int i=0; i < ile; i++) {
+        
+        cout << "Podaj liczbę: ";
+        //cin >> t[i];
+        cout << "Adres elementu: " << (t + i) << endl;
+        cin >> *(t + i);
+        
+    }
+}
+
+void drukuj(float *t, int ile) {
+    for(int i=0; i < ile; i++) {
+        cout << *(t + i) << endl;
+    }
+}
+
 int tab1W(){
     
-    int *ptab = NULL;
     cout << "Ile podasz liczb?";
     int ile;
     cin >> ile;
     
+    cout << sizeof(ile) << endl;
+    -
     try {
-        ptab = new int[ile]; //utworzenie tablicy dynamicznej
+        float *ptab = NULL;
+        ptab = new float[ile]; //utworzenie tablicy dynamicznej
+        wprowadz(ptab, ile);
+        drukuj(ptab, ile);
+        
     } catch(bad_alloc) {
         cout<< "Za mało pamięci";
         return 1;
     }
-
+    return 0;
 }
+
+int tab2W(){
+    int w, k, ,i ,j;
+    cout << "Podaj liczbę wierszy i kolumn: ";
+    cin >> w >> k;
+    int **tab;  //deklaracja wskaźnika do wskaźnika
+    
+    try {
+        tab = new int * [w]; //utworzenie tablicy dynamicznej
+        wprowadz(ptab, ile);
+        drukuj(ptab, ile);
+        
+    } catch(bad_alloc) {
+        cout<< "Za mało pamięci";
+        return 1;
+    }
+    return 0;
+}
+
+
 int main(int argc, char **argv)
 {
 	tab1W();
