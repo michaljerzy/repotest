9fd5094529bbac8cafcdc697cf10bfd07daa638f
michaljerzy <michaljerzy99@gmail.com>
2018-01-24 13:26:35 +0100

    .

---
+++ kody/cpp/stos_tab.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+bool czy_pusty(int sp, int sr) {
+    if (sp < sr) return true;
+    return false;
+    }
+
+void push(int *stos, int &sp, int sr, int dane) {
+    
+    cout << dane << " "; //informacyjny wydruk wartości 
+    
+    if (czy_pusty(sp, sr)) {
+        stos[sp] = dane; 
+        sp++;
+    } else {
+        cout << "Błąd przepełnienia stosu!";
+    }
+
+}   
+
+
+int pop(int stos[], int &sp){
+    
+    sp--;
+    return stos[sp];
+} 
+
+int main(int argc, char **argv)
+{
+    int *stack; //wskaźnik 
+    int sr; //rozmiar stosu 
+    int sp = 0; //wskaźnik stosu (ang. stack pointer)
+    
+    
+    cout<< "Podaj rozmiar: "; cin >> sr;
+    stack = new int[sr];
+    
+    srand(time(NULL));
+    for (int i=0; i < sr; i++){
+        push(stack, sp, sr, rand()%100 + 1);
+    
+    }
+     
+	cout<< endl;
+    
+    for (int i=0; i < sr; i++){
+        cout << pop(stack, sp) << " ";
+    
+    }
+     
+	return 0;
+}
+
